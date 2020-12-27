2d3f1f038a0fb8af46e11127ccb6fb351fedde66
michLem <mjlemiec@gmail.com>
2020-12-27 22:40:56 +0100

    Create controllers and models, move files

---
+++ RESTComponents/RESTComponents/Models/IPlayerList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTComponents.Models
+{
+    public interface IPlayerList
+    {
+        // Add new player at the beginning 
+        bool AddPlayer(Player newPlayer);
+        // Get all players
+        List<Player> GetPlayers();
+        // Searches through players' list to find the right player 
+        Player Find(int key);
+        // Removes player when all teritories are lost
+        Player Remove(int key);
+    }
+}
---
+++ RESTComponents/RESTComponents/Models/PlayerList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTComponents.Models
+{
+    public class PlayerList :IPlayerList
+    {
+        Dictionary<int, Player> players = new Dictionary<int, Player>();
+        int currentId = 0;
+        Object locker = new Object();
+        public bool AddPlayer(Player newPlayer)
+        {
+            lock (locker)
+            {
+                newPlayer.Id = currentId;
+                players.Add(currentId++, newPlayer);
+                return true;
+            }
+        }
+
+        public Player Find(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Player> GetPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player Remove(int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
